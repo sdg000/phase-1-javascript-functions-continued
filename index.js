d924a3f99df122ea994604a23047f4f815214296
Seth David Gyimah <esec26@gmail.com>
2022-06-08 15:46:09 +0000

    phase-1- javascript-functions-continued

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(toDo = "go to the office"){
+    return `This Monday, I will ${toDo}.`
+
+}
+
+function wrapAdjective(string = "*"){
+
+    return function (inner = "special"){
+        return `You are ${string}${inner}${string}!`
+    }
+
+    //console.log()
+    //return `function (${string}${inner}${string})`
+
+}
